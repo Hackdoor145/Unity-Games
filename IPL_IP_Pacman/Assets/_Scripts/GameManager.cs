c00269c7c8847bc5a6d97d423f01b26724b81871
Hackdoor145 <miguel.alex.salgueiro@outlook.pt>
2017-10-01 21:03:16 +0100

    Adicionado Movimento ao jogador
    Foi adicionado movimento ao jogador e alterado alguns prefabs.

--- IPL_IP_Pacman/Assets/_Scripts/GameManager.cs
+++ IPL_IP_Pacman/Assets/_Scripts/GameManager.cs
@@ -41,14 +41,11 @@ public class GameManager : MonoBehaviour {
     public GameObject comida;
     
 
-    public float speed;             //Floating point variable to store the player's movement speed.
-
-    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    
 
 
     // Use this for initialization
     void Start() {
-        rb2d = GetComponent<Rigidbody2D>();
 
         float mapaLinhas = tileMatrix.GetLength(0) / 2;
         float mapaColunas = tileMatrix.GetLength(1) / 2;
---
+++ IPL_IP_Pacman/Assets/_Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float velocidade;
+    public Rigidbody2D rb2D;
+
+    // Use this for initialization
+    void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimento do Jogador
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * velocidade * Time.deltaTime;
+            rb2D.MoveRotation(rb2D.rotation + velocidade * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * velocidade * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * velocidade * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * velocidade * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Bateu!");
+        transform.position += new Vector3(0, 0, 0);
+    }
+}
