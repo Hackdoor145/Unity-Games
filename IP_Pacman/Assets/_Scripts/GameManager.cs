ce37fe0a40be804e2e9978b9e53927676deaf2f8
Hackdoor145 <miguel.alex.salgueiro@outlook.pt>
2017-10-19 19:05:34 +0100

    quase acabado
    quase acabado falta inimigos e outras coisas relacionadas com colisoes

---
+++ IP_Pacman/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public int[,] tileMatrix = {
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+        { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+        { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+        { 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+        { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+        { 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1 },
+        { 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1 },
+        { 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+        { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+    };
+
+    [SerializeField]
+    private GameObject wall; //block prefab
+    [SerializeField]
+    private GameObject square;
+    [SerializeField]
+    private GameObject food;
+    [SerializeField]
+    private GameObject powerFood;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private Transform blocksHolder;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        float numeroLinhas = tileMatrix.GetLength(0);
+        float numeroColunas = tileMatrix.GetLength(1);
+        float mapaLinhas = numeroLinhas / 2;
+        float mapaColunas = numeroColunas / 2;
+
+        for (int i = 0; i < numeroLinhas; i++)
+        {
+            for (int j = 0; j < numeroColunas; j++)
+            {
+                if (tileMatrix[i, j] == 1)
+                {
+
+                    Instantiate
+                        (
+                        wall,
+                        new Vector3(j - mapaLinhas + 2, i - mapaColunas, 0),
+                        Quaternion.identity,
+                        blocksHolder
+                        );
+
+                }
+                else if (tileMatrix[i, j] == 0)
+                {
+
+                    if ((i == 1 && j == 1) || (i == 1 && j == 26) || (i == 29 && j == 26) || (i == 29 && j == 1))
+                    {
+
+                        Instantiate
+                            (
+                            powerFood,
+                            new Vector3(j - mapaLinhas + 2, i - mapaColunas, 0),
+                            Quaternion.identity
+                            );
+
+                        Instantiate
+                            (
+                            square,
+                            new Vector3(j - mapaLinhas + 2, i - mapaColunas, 0),
+                            Quaternion.identity
+                            );
+
+                    }
+                    else
+                    {
+
+                        Instantiate(
+                            square,
+                            new Vector3(j - mapaLinhas + 2, i - mapaColunas, 0),
+                            Quaternion.identity
+                            );
+                        Instantiate(
+                            food,
+                            new Vector3(j - mapaLinhas + 2, i - mapaColunas, 0),
+                            Quaternion.identity
+                            );
+
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool IsEmptyPosition(int playerX,int playerY)
+    {
+        
+        for (int i = 0; i < tileMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < tileMatrix.GetLength(1); j++)
+            {
+                
+
+            }
+        }
+        return false;
+    }
+}
+
---
+++ IP_Pacman/Assets/_Scripts/PlayerScript.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    //playerSpeed de movimento do jogador
+    public float playerSpeed;
+    //score
+    private int score;
+    //direction N-norte, S-sul, O-oeste, E-este
+    private char direction = 'O';
+    //position of the player at the moment
+    private Vector3 playerPosition;
+    //referencia ao script gameManager
+    public GameManager gameManager;
+
+    int positionX;
+    int positionY;
+
+    private enum State
+    {
+        Dead,
+        Alive
+    }
+
+    private enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        playerPosition = gameObject.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //movimento automático, apenas se escolhe a direção
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameManager.IsEmptyPosition(positionX, positionY);
+            direction = 'O';
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))   
+        {
+            //podeAndar = true;
+            direction = 'E';
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //podeAndar = true;
+            direction = 'N';
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            // podeAndar = true;
+            direction = 'S';
+        }
+        switch (direction)
+        {
+            case 'O':
+                // if (podeAndar)
+                //{
+                transform.position += Vector3.left * playerSpeed * Time.deltaTime;
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                playerPosition = gameObject.transform.position;
+                //}
+
+                break;
+            case 'E':
+                //if (podeAndar)
+                // {
+                transform.position += Vector3.right * playerSpeed * Time.deltaTime;
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                playerPosition = gameObject.transform.position;
+                //}
+
+                break;
+            case 'N':
+                //if (podeAndar)
+                // {
+                transform.position += Vector3.up * playerSpeed * Time.deltaTime;
+                transform.rotation = Quaternion.Euler(0, 0, -90);
+                playerPosition = gameObject.transform.position;
+                // }
+
+                break;
+            case 'S':
+                //if (podeAndar)
+                //{
+                transform.position += Vector3.down * playerSpeed * Time.deltaTime;
+                transform.rotation = Quaternion.Euler(0, 0, 90);
+                playerPosition = gameObject.transform.position;
+                //}
+
+                break;
+        }
+        positionX = (int)transform.position.x;
+        positionY = (int)transform.position.y;
+        Debug.Log(playerPosition);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //comeu?
+        if (collision.gameObject.tag == "Food")
+        {
+            Destroy(collision.gameObject);
+            score += 100;
+            //Debug.Log(score);
+        }
+        else if (collision.gameObject.tag == "PowerFood")
+        {
+            Destroy(collision.gameObject);
+            score += 500;
+        }
+
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Obstacle")
+    //    {
+    //        podeAndar = false;
+    //        Debug.Log("bateu");
+    //    }
+    //}
+}
